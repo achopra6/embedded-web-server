4b0e1bf61c21a51d680ce8dd82f8902cc7da1a36
achopra6 <achopra6@masonlive.gmu.edu>
2017-07-23 01:32:30 -0400

    added main.c

--- Makefile
+++ Makefile
@@ -1,9 +1,8 @@
-EXEC_NAME = rpi_action_cam
+EXEC_NAME = server
 
-INC_DIRS = 
 
 SRC_FILES = src/main.c
 
 all:
-	gcc $(SRC_FILES) $(INC_DIRS) -o $(EXEC_NAME) -g -Wdeprecated-declarations `pkg-config --cflags`
+	gcc $(SRC_FILES) -o $(EXEC_NAME) -lmicrohttpd
 
--- src/main.c
+++ src/main.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
-#include <errno.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <microhttpd.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <semaphore.h>
-#include <stddef.h>
-
 /**
 * @file main.c
-* @brief Contains routines to create multi threads
+* @brief routines for embedded web server
 * @author Abhimanyu Chopra
 */
 
+#define PORT 80
+
+static int answer_to_connection (void *cls, struct MHD_Connection *connection,
+									const char *url, const char *method,
+									const char *version, const char *upload_data,
+									size_t *upload_data_size, void **con_cls){
+
+	const char *page = "<html><body>Hello, browser!</body></html>";
+	struct MHD_Response *response;
+	int ret;
+	response = MHD_create_response_from_buffer (strlen (page), (void *) page, MHD_RESPMEM_PERSISTENT);
+	ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
+	MHD_destroy_response (response);
+	return ret;
+}
 
 
 /**
@@ -22,8 +33,12 @@
 */
 int main(int argc, char *argv[])
 {
-	
-
+	struct MHD_Daemon *daemon;
+	daemon = MHD_start_daemon (MHD_USE_SELECT_INTERNALLY, PORT, NULL, NULL, &answer_to_connection, NULL, MHD_OPTION_END);
+	if (NULL == daemon)
+		return 1;
+	(void) getchar ();
+	MHD_stop_daemon (daemon);
 	return 0;
 }
 
